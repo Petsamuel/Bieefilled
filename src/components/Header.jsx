3d0d8b232d18327b8839a07f979f1c0143c3fed1
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2023-04-15 20:09:08 +0100

    chore: made minor adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ function Header() {
           <div class="flex items-center md:order-2">
             <a
               href="/blog"
-              class="text-white  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 focus:outline-none"
+              class="text-white  focus:ring-1  font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 focus:outline-none hover:shadow-sm "
             >
               Blog
             </a>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -94,14 +94,15 @@ function Hero() {
                   RESUME
                 </span>
               </button>
-              <button
+              <a
+                href="/contact"
                 className="relative inline-flex items-center ju
               stify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-sm"
               >
                 <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-[#242424] dark:bg-[#242424] rounded-md group-hover:bg-opacity-0 text-white">
-                  <a href="/contact">QUOTES</a>
+                  QUOTES
                 </span>
-              </button>
+              </a>
             </div>
           </div>
         </div>
