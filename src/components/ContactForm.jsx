bab0646d4a0ff41c64cb4a7cf91bd70151e23553
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2023-04-26 21:49:04 +0100

    chore: minor change

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,50 +4,59 @@ import emailjs from "@emailjs/browser";
 import Loading from "./Loading";
 import ReCAPTCHA from "react-google-recaptcha";
 import { secret_key } from "./store/data";
-const InputField = ({ type, label, name, handleChange }) => {
+
+export const InputField = ({ type, label, name, handleChange, isTextArea }) => {
   return (
     <>
       <div class="relative">
-        <input
-          type={type}
-          name={name}
-          class="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent bg-black dark:bg-black rounded-lg border-1  outline-none border shadow-sm border-gray-600 appearance-none text-white dark:text-white dark:border-gray-700 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-gray-900 peer"
-          placeholder=" "
-          required
-          minLength="4"
-          onChange={handleChange}
-        />
-        <label
-          required
-          class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-black dark:bg-black px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-        >
-          {label}
-        </label>
+        {!isTextArea ? (
+          <>
+            {" "}
+            <input
+              type={type}
+              name={name}
+              class="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent bg-black dark:bg-black rounded-lg border-1  outline-none border shadow-sm border-gray-600 appearance-none text-white dark:text-white dark:border-gray-700 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-gray-900 peer"
+              placeholder=" "
+              required
+              minLength="4"
+              // value=""
+              onChange={handleChange}
+            />
+            <label
+              required
+              class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-black dark:bg-black px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+            >
+              {label}
+            </label>
+          </>
+        ) : (
+          <>
+            {" "}
+            <label className="absolute text-sm text-white dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-black dark:bg-black px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1 block">
+              {label}
+            </label>
+            <textarea
+              name={name}
+              required
+              placeholder=" "
+              // value=""
+              onChange={handleChange}
+              className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border border-gray-600 focus:border-indigo-600 shadow-sm rounded-lg"
+            ></textarea>
+          </>
+        )}
       </div>
     </>
-  );
-};
-const TextField = ({ name, handleChange }) => {
-  return (
-    <div className="relative">
-      <label className="absolute text-sm text-white dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-black dark:bg-black px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1 block">
-        Message
-      </label>
-      <textarea
-        name={name}
-        required
-        placeholder=" "
-        onChange={handleChange}
-        className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border border-gray-600 focus:border-indigo-600 shadow-sm rounded-lg"
-      ></textarea>
-    </div>
   );
 };
 
 export const ContactForm = () => {
   const [status, setStatus] = useState(false);
   const [alertMessage, setAlertMessage] = useState(null);
-  const [formValue, setFormValue] = useState({ name: "" });
+  const [formValue, setFormValue] = useState({
+    name: "",
+    description: "",
+  });
 
   const recaptchaRef = createRef();
   const form = useRef();
@@ -71,36 +80,7 @@ export const ContactForm = () => {
 
   const SubmitForm = (e) => {
     e.preventDefault();
-    // recaptchaRef.current.execute();
-
-    // recaptcher v2
-    const SubmitCaptcha = (captchaValue) => {
-      const params = {
-        ...formValue,
-        "g-recaptcha-response": captchaValue,
-      };
-    };
     console.log(formValue);
-
-    emailjs
-      .sendForm(
-        secret_key.mail_service_id,
-        secret_key.mail_template_id,
-        form.current,
-        secret_key.mail_public_key
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setStatus(false);
-          setAlertMessage(true);
-        },
-        (error) => {
-          console.log(error.text);
-          setAlertMessage(false);
-          setStatus(!status);
-        }
-      );
   };
   return (
     <>
@@ -121,38 +101,43 @@ export const ContactForm = () => {
             type="text"
             label="Full Name"
             name="user_name"
+            // value={value}
             onChange={(e) => {
-              setFormValue({ [e.target.name]: e.target.value });
+              console.log(e.target.value);
             }}
           />
           <InputField
             type="email"
             label="Email"
             name="user_email"
+            // value={value}
             onchange={(e) => {
-              setFormValue({ [e.target.name]: e.target.value });
+              console.log(e.target.value);
             }}
           />
         </div>
-        <TextField
+        <InputField
+          isTextArea
+          label="Message"
           name="user_message"
+          // value={value}
           onChange={(e) => {
-            setFormValue({ [e.target.name]: e.target.value });
+            console.log(e.target.value);
           }}
         />
-        <ReCAPTCHA
+        {/* <ReCAPTCHA
           ref={recaptchaRef}
           theme="dark"
           sitekey={secret_key.reCAPTCHA}
           onChange={() => {
             SubmitCaptcha;
           }}
-        />
+        /> */}
 
         <button
           className="w-full px-4 py-2 text-white  font-medium bg-gradient-to-r from-purple-600 to-blue-500 hover:first-line:bg-gradient-to-l hover:from-purple-600 hover:to-fuchsia-500 active:bg-indigo-600 rounded-lg duration-150 "
-          onClick={() => {
-            SubmitForm;
+          onClick={(e) => {
+            SubmitForm(e);
           }}
         >
           {!status ? `Submit` : <Loading />}
@@ -172,7 +157,14 @@ export const NewsletterForm = () => {
           >
             Sign up for our newsletter
           </label>
-          <InputField type="email" label="Email" />
+          <InputField
+            name="user_email"
+            type="email"
+            label="Email"
+            onchange={(e) => {
+              e.target.value;
+            }}
+          />
           <button
             type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
