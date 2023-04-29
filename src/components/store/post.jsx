fcac2169baf82729122c615d3b12b23f9aa7863e
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2023-04-29 21:22:17 +0100

    modified post

--- src/components/store/post.jsx
+++ src/components/store/post.jsx
@@ -1,11 +1,19 @@
 export const Post = [
   {
     title:
-      "Secure Your Website with reCaptcha and EmailJS: A Comprehensive Tutorial for Web Developers",
+      "How to Create a Secure Contact Form for Your Website using reCAPTCHA and EmailJS",
     featured_img:
       "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     description:
       "You don’t need to be an expert in web development or security to follow along — we’ll guide you through the process step-by-step.",
     href: "https://bieefilled.medium.com/secure-your-website-with-recaptcha-and-emailjs-a-comprehensive-tutorial-for-web-developers-6f3d675259ac",
+  },
+  {
+    title: "How to Create a Custom 404 Error Page with HTML and CSS",
+    featured_img:
+      "https://miro.medium.com/v2/resize:fit:640/format:webp/1*PfEFlM13WNZljHngCqdd5A.png",
+    description:
+      "Are you tired of the plain and uninformative 404 error pages on your website? we'll guide you through the process of creating a custom 404 error page using HTML and CSS",
+    href: "https://bieefilled.medium.com/designing-html-error-page-dd68dd38045f",
   },
 ];
