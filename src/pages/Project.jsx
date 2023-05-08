15d7a2b2a78dd3e6a2ccf9a9f67f453dd1c8705e
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2023-05-08 09:49:09 +0100

    fix: irregularities

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,13 +2,12 @@ import { projects } from "../components/store/data";
 import Card from "../components/Card";
 import GitHubCalendar from "react-github-calendar";
 
-
 function Project() {
   return (
     <>
       <section className="flex flex-col items-center">
         <h1 className="py-4 px-2 mb-3  text-semibold font-[Poppins] text-2xl text-white  mt-4">
-          <span class="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-500 border-b-2">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-500 border-b-2">
             Projects&nbsp;
           </span>
         </h1>
@@ -17,7 +16,7 @@ function Project() {
 
         <div className="flex flex-col items-center">
           <h1 className="py-4 px-2 mb-3  text-semibold font-[Poppins] text-2xl text-white  mt-4">
-            <span class="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-500 border-b-2">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-500 border-b-2">
               Open Source
             </span>
             &nbsp; Activities
