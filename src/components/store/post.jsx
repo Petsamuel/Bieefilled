66b7298c8c82ca068bbe019192c537340e9d78e3
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2023-05-08 09:55:46 +0100

    fix: irregularities

--- src/components/store/post.jsx
+++ src/components/store/post.jsx
@@ -5,15 +5,23 @@ export const Post = [
     featured_img:
       "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     description:
-      "You don’t need to be an expert in web development or security to follow along — we’ll guide you through the process step-by-step.",
+      "You don’t need to be an expert in web development or security to follow along — we’ll guide you through the process step-by-step...",
     href: "https://bieefilled.medium.com/secure-your-website-with-recaptcha-and-emailjs-a-comprehensive-tutorial-for-web-developers-6f3d675259ac",
+  },
+  {
+    title: "Building and Publishing a React Component with Vite and NPM",
+    featured_img:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--VPKU2iTm--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/sbmfgi239qg1aie9qgtz.jpg",
+    description:
+      "React has become one of the most popular front-end frameworks due to its component-based architecture, and with the ease of sharing packages through Npm...",
+    href: "https://dev.to/bieefilled/building-and-publishing-a-react-component-with-vite-and-npm-1npd",
   },
   {
     title: "How to Create a Custom 404 Error Page with HTML and CSS",
     featured_img:
       "https://miro.medium.com/v2/resize:fit:640/format:webp/1*PfEFlM13WNZljHngCqdd5A.png",
     description:
-      "Are you tired of the plain and uninformative 404 error pages on your website? we'll guide you through the process of creating a custom 404 error page using HTML and CSS",
+      "Are you tired of the plain and uninformative 404 error pages on your website? we'll guide you through the process of creating a custom 404 error page using HTML and CSS...",
     href: "https://bieefilled.medium.com/designing-html-error-page-dd68dd38045f",
   },
 ];
