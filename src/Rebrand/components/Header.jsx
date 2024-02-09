52fdbed6ffa649f007bb78c6e5be344954f7482f
petsamuel <petsamuel4@gmail.com>
2024-02-09 21:59:40 +0100

    rebrand

--- src/Rebrand/components/Header.jsx
+++ src/Rebrand/components/Header.jsx
@@ -5,7 +5,7 @@ import { FaLinkedinIn } from "react-icons/fa6";
 
 export const Header = () => {
     return (
-        <section className="flex justify-between lg:py-2 py-8 items-center">
+        <section className="flex justify-between lg:py-2 py-8 lg:items-center ">
             <div className="flex flex-col mb-4 gap-y-1">
                 <img src={img} alt="img" className="rounded-full h-auto w-[30%]" />
                 <div className=" font-black text-3xl"> Samuel Peters </div>
@@ -18,12 +18,12 @@ export const Header = () => {
                          font-medium rounded-full text-green-900 py-2 transition-all ease-in-out ring-1  ring-gray-300 hover:scale-95 active:scale-90"
                         href={"Https://linkedIn.com/in/bieefilled"}
                     >
-                        <span className=" items-center gap-2 flex relative px-5  transition-all ease-in duration-75  rounded-md group-hover:bg-opacity-0 text-green-900 font-bold lg:text-lg text-xs">
+                        <span className=" w-24 lg:w-full items-center gap-2 flex relative px-5  transition-all ease-in duration-75  rounded-md group-hover:bg-opacity-0 text-green-900 font-bold lg:text-lg text-xs">
                             Hire Me
                         </span>
                     </a>
                 </div>
-                <div className=" lg:block hidden">
+                <div className="lg:block hidden">
                     <a
                         className="  gap-3 relative inline-flex  justify-center p-0.5 mb-2 mr-2 overflow-hidden 
                          font-medium  rounded-full text-green-900 py-2 transition-all ease-in-out ring-1 ring-gray-300  hover:scale-95 active:scale-90"
