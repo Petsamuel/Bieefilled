77ce5d7d5659ae32a523a12ece9590a393114330
petsamuel <petsamuel4@gmail.com>
2024-03-04 01:41:53 +0100

    modified:   src/Rebrand/components/Stacks.jsx 	modified:   src/components/Loading.jsx

--- src/Rebrand/components/Stacks.jsx
+++ src/Rebrand/components/Stacks.jsx
@@ -4,9 +4,9 @@ import { Stack } from "../../components/store/data"
 export const Stacks = () => {
 
     return (
-        <div className="flex flex-wrap lg:gap-2  gap-2.5 justify-start ">{Stack.map((value, key) => (
+        <div className="flex flex-wrap lg:gap-2 gap-2.5 justify-start gap-x-3 gap-y-2">{Stack.map((value, key) => (
             <div className='flex rounded-full border py-2 hover:scale-95 active:scale-90 gap-2'>
-                <div key={key} className="flex mx-4 cursor-pointer items-center font-['Poppins'] gap-1 ">
+                <div key={key} className="flex mx-4 cursor-pointer items-center font-['Poppins'] gap-2.5 ">
                     <div className='flex gap-x-1 items-center max-w-content lg:text-3xl text-md md:text-md'>{value.icon}</div>
                     <div className="lg:text-md md:text-sm text-[.66em]">{value.name}</div>
                 </div>
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,7 +5,8 @@ function Loading() {
   return (
     <div role="status" className="flex items-center place-content-center h-screen w-screen relative">
       <div className="flex-1 text-center pt-5 justify-center items-center flex relative flex-col">
-        <img src={smile} alt="loader" class="w-[30%] lg:w-[25%]" /> <p className="brandname lg:text-4xl text-2xl animate-pulse">LOADING...</p>
+        <img src={smile} alt="loader" className="w-[30%] lg:w-[25%]" />
+        <p className="brandname lg:text-4xl text-2xl animate-pulse">LOADING...</p>
       </div>
     </div>
   );
