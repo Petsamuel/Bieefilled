4418c07f8eafeec1afdb555fccce4af28e964323
petsamuel <petsamuel4@gmail.com>
2026-06-12 20:07:14 +0100

    feat: add Stacks component to display technology stack items with animated borders

--- src/Rebrand/components/Stacks.jsx
+++ src/Rebrand/components/Stacks.jsx
@@ -7,7 +7,7 @@ export const Stacks = () => {
         <div className="lg:grid lg:grid-cols-4 grid-cols-2 lg:gap-3 gap-2.5 justify-start gap-x-4 gap-y-2 hidden">{Stack.map((value, index) => (
             <div className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50" key={index}>
                 
-                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#ffffff_0%,#000000_50%,#ffffff_100%)]" />
                 <div className="inline-flex gap-2 h-full w-full cursor-pointer items-center justify-center rounded-full p-4 text-sm font-medium  text-black backdrop-blur-3xl bg-white ">
                     <div className='flex gap-x-1 items-center max-w-content lg:text-3xl text-md md:text-md'>{value.icon}</div><div className="lg:text-md md:text-sm text-[.66em]">{value.name}</div>
                 </div>
